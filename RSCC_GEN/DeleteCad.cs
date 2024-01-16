626b12b7193846c16c902004419dec913d9b8bc9
oelshaf3y <o.elshaf3y@gmail.com>
2024-01-17 00:03:57 +0200

    init

---
+++ RSCC_GEN/DeleteCad.cs
@@ -0,0 +1,36 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace RSCC_GEN
+{
+    internal class DeleteCad : IExternalCommand
+    {
+        UIDocument uidoc;
+        Document doc;
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            uidoc = commandData.Application.ActiveUIDocument;
+            doc = uidoc.Document;
+
+
+            FilteredElementCollector fec = new FilteredElementCollector(doc).OfClass(typeof(CADLinkType));
+            int count = fec.Count();
+            if (count == 0) { TaskDialog.Show("Info", "No more DWG Imports In The Project."); return Result.Succeeded; }
+            else
+            {
+                DialogResult dia = MessageBox.Show($"Are You Sure You Want To Delete {count} CAD Files?\nThis CAN NOT BE UNDONE!", "Are You Sure?", MessageBoxButtons.YesNo);
+                if (dia == DialogResult.No) return Result.Cancelled;
+            }
+
+            Transaction tr = new Transaction(doc, "Delete CAD Imports");
+            tr.Start();
+            doc.Delete(fec.Select(x => x.Id).ToArray());
+            TaskDialog.Show("Done", $"Successfully deleted {count} CAD Files.");
+            tr.Commit();
+            tr.Dispose();
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ RSCC_GEN/RebarSet.cs
@@ -0,0 +1,64 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Structure;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RSCC_GEN
+{
+    [TransactionAttribute(TransactionMode.Manual)]
+    internal class RebarSet : IExternalCommand
+    {
+        UIDocument uidoc;
+        Document doc;
+        List<ElementId> rebarSets;
+        StringBuilder sb;
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            sb = new StringBuilder();
+            uidoc = commandData.Application.ActiveUIDocument;
+            doc = uidoc.Document;
+            rebarSets = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Rebar)
+                .WhereElementIsNotElementType().ToElementIds().ToList();
+            using (Transaction tr = new Transaction(doc, "Rebar magic"))
+            {
+                tr.Start();
+                foreach (ElementId id in rebarSets)
+                {
+                    try
+                    {
+
+                        var t = doc.GetElement(id);
+                        Rebar r = t as Rebar;
+                        ElementId rebNumberId = r.LookupParameter("Rebar Number").Id;
+                        ElementId barLengthId = r.LookupParameter("Bar Length").Id;
+                        StringParameterValue startNum = r.GetParameterValueAtIndex(rebNumberId, 0) as StringParameterValue;
+                        StringParameterValue endNum = r.GetParameterValueAtIndex(rebNumberId, r.Quantity - 1) as StringParameterValue;
+                        DoubleParameterValue minLength = r.GetParameterValueAtIndex(barLengthId, 0) as DoubleParameterValue;
+                        DoubleParameterValue maxLength = r.GetParameterValueAtIndex(barLengthId, r.Quantity - 1) as DoubleParameterValue;
+                        r.LookupParameter("RSCC_MaxLength").Set(Math.Max(minLength.Value, maxLength.Value));
+                        r.LookupParameter("RSCC_MinLength").Set(Math.Min(minLength.Value, maxLength.Value));
+                        r.LookupParameter("RSCC_SetRebNum1").Set(startNum.Value);
+                        r.LookupParameter("RSCC_SetRebNum2").Set(endNum.Value);
+                        r.LookupParameter("RSCC_SetQTY").Set(r.Quantity);
+                    }
+                    catch
+                    {
+
+                    }
+                }
+                tr.Commit();
+                tr.Dispose();
+            }
+            return Result.Succeeded;
+
+        }
+        void td(string mes)
+        {
+            TaskDialog.Show("info", mes);
+        }
+    }
+}
